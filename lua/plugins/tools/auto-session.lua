c44f62ca707859ae00fbcfa492a6c2c66df18653
Huan Chen <huan.chen0217@gmail.com>
2025-10-13 14:53:18 -0500

    final change

--- lua/plugins/tools/auto-session.lua
+++ lua/plugins/tools/auto-session.lua
@@ -4,10 +4,13 @@ return {
   config = function()
     require('auto-session').setup {
       log_level = 'error',
-      auto_session_enable_last_session = true,
-      auto_session_root_dir = vim.fn.stdpath('data')..'/sessions/',
-      auto_session_enabled = true,
+      auto_session_enabled = true,               -- still auto-save sessions
+      auto_session_enable_last_session = false,  -- disable auto-restore
+      auto_session_root_dir = vim.fn.stdpath('data') .. '/sessions/',
       pre_save_cmds = { "lua vim.lsp.buf.format({ async = false })" },
     }
+
+    -- Optional keybinding to restore manually
+    vim.keymap.set('n', '<leader>rs', ':RestoreSession<CR>', { desc = 'Restore Session' })
   end,
 }
