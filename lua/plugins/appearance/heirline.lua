c81169c38545eb7f65d36ec831b0197b1a3e9ca4
Huan Chen <huan.chen0217@gmail.com>
2025-10-13 14:35:09 -0500

    bruh

--- lua/plugins/appearance/heirline.lua
+++ lua/plugins/appearance/heirline.lua
@@ -1,33 +1,150 @@
+-- File: lua/plugins/appearance/heirline.lua
 return {
   "rebelot/heirline.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     local conditions = require("heirline.conditions")
-    local Heirline = require("heirline")
+    local utils = require("heirline.utils")
+    local get_hl = utils.get_highlight
 
-    Heirline.setup({
-      statusline = {
-        -- Mode
-        { provider = function() return " " .. vim.fn.mode():upper() end, hl = { fg = "green" } },
+    -- Colors
+    local colors = {
+      blue = "#7aa2f7",
+      green = "#9ece6a",
+      red = "#f7768e",
+      yellow = "#e0af68",
+      purple = "#9d7cd8",
+      cyan = "#7dcfff",
+      gray = "#5c6370",
+    }
 
-        -- Git branch
-        { provider = "git_branch", hl = { fg = "orange" } },
+    -- Mode highlight
+    local mode_colors = {
+      n = colors.red,
+      i = colors.green,
+      v = colors.blue,
+      V = colors.blue,
+      ["\22"] = colors.blue,
+      c = colors.yellow,
+      s = colors.orange,
+      R = colors.purple,
+      t = colors.cyan,
+    }
 
-        -- Diagnostics
-        { provider = "diagnostics", hl = { fg = "red" } },
+    local function get_mode_color()
+      return mode_colors[vim.fn.mode()] or colors.gray
+    end
 
-        -- Filename
-        { provider = "file_name", hl = { fg = "blue" }, 
-          truncation = { priority = 1 }
-        },
+    -- Statusline components
+    local StatusLines = {
+      -- Left section
+      {
+        provider = function()
+          return " " .. vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+        end,
+        hl = { fg = colors.blue, bold = true },
+        right_sep = "",
+      },
+      {
+        provider = function()
+          local mode = vim.fn.mode()
+          local alias = {
+            n = "NORMAL",
+            i = "INSERT",
+            v = "VISUAL",
+            V = "V-LINE",
+            ["\22"] = "V-BLOCK",
+            c = "COMMAND",
+            s = "SELECT",
+            R = "REPLACE",
+            t = "TERMINAL",
+          }
+          return " " .. (alias[mode] or mode) .. " "
+        end,
+        hl = function()
+          return { fg = get_mode_color(), bold = true, bg = colors.gray }
+        end,
+        left_sep = "",
+        right_sep = "",
+      },
+      -- Git branch
+      {
+        provider = "git_branch",
+        hl = { fg = colors.purple, bold = true },
+        icon = " ",
+        left_sep = " ",
+      },
+      -- Diff
+      {
+        condition = conditions.is_git_repo,
+        provider = "git_diff_added",
+        hl = { fg = colors.green },
+        icon = "  ",
+        left_sep = " ",
+      },
+      {
+        condition = conditions.is_git_repo,
+        provider = "git_diff_removed",
+        hl = { fg = colors.red },
+        icon = "  ",
+        left_sep = " ",
+      },
+      {
+        condition = conditions.is_git_repo,
+        provider = "git_diff_changed",
+        hl = { fg = colors.yellow },
+        icon = " 柳",
+        left_sep = " ",
+      },
 
-        -- Fill space
-        { provider = "%=", },
+      -- Middle section: filename
+      {
+        provider = function()
+          local file = vim.fn.expand("%:t")
+          if file == "" then
+            return "[No Name]"
+          else
+            return file
+          end
+        end,
+        hl = { fg = colors.cyan, bold = true },
+        left_sep = "",
+        right_sep = "",
+      },
 
-        -- File info
-        { provider = "file_type", hl = { fg = "yellow" } },
-        { provider = "position", hl = { fg = "cyan" } },
-      }
+      -- Diagnostics
+      {
+        condition = conditions.has_diagnostics,
+        provider = "diagnostics",
+        hl = { fg = colors.red },
+        icon = " ",
+        left_sep = " ",
+        right_sep = " ",
+      },
+
+      -- Right section
+      {
+        provider = "filetype",
+        hl = { fg = colors.yellow, bold = true },
+        left_sep = "",
+        right_sep = "",
+      },
+      {
+        provider = "progress",
+        hl = { fg = colors.blue, bold = true },
+        left_sep = " ",
+      },
+      {
+        provider = "location",
+        hl = { fg = colors.green, bold = true },
+        left_sep = " ",
+      },
+    }
+
+    -- Setup Heirline
+    require("heirline").setup({
+      statusline = StatusLines,
+      opts = { colors = colors, disable_winbar_cb = function() return false end },
     })
   end,
 }
